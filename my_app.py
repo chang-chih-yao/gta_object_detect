6f17abecf7552d4ef569a120ea8fd8d0b0a8650f
張之耀(Chih-Yao, Chang) <m900054@gmail.com>
2022-07-17 18:10:07 +0800

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -3,7 +3,7 @@ import os
 import time
 import tkinter as tk
 from threading import Thread
-from cv2 import VideoCapture, rectangle, putText, FONT_HERSHEY_SIMPLEX, cvtColor, COLOR_RGB2BGR, COLOR_BGR2RGB, TrackerCSRT_create
+from cv2 import VideoCapture, rectangle, putText, FONT_HERSHEY_SIMPLEX, cvtColor, COLOR_RGB2BGR, COLOR_BGR2RGB, TrackerCSRT_create, resize, INTER_AREA
 from PIL import ImageTk, Image
 import pydirectinput as pd
 from pyautogui import screenshot, moveRel
@@ -26,7 +26,7 @@ model.amp = False  # Automatic Mixed Precision (AMP) inference
 
 tracker = TrackerCSRT_create()
 
-detect_time = 451
+detect_time = 102
 start_time = 0   # for GUI timer
 start_flag = False
 end_game = False
@@ -46,14 +46,14 @@ gta_ = gw.getWindowsWithTitle('FiveM')[0]
 offset = 10
 left = gta_.left + offset
 top = gta_.top + offset+20
-w = 800
-h = 600
+w = 1280
+h = 720
 print(left, top, w, h)
 
-human_x1 = 335
-human_x2 = 372
-human_y1 = 255
-human_y2 = 440
+human_x1 = 563
+human_x2 = 617
+human_y1 = 325
+human_y2 = 530
 
 target_x1 = 0
 target_x2 = 0
@@ -76,20 +76,20 @@ class my_keyboard:
         self.s_key = False
         self.d_key = False
 
-    def press_e(self):
-        self.release_all()
-        print('press e')
-        pd.press('e')
+    # def press_e(self):
+    #     self.release_all()
+    #     print('press e')
+    #     pd.press('e')
 
     def l(self):
         if not self.a_key:
-            self.release_all()
+            self.release_left_right()
             self.a_key = True
             print('go left')
             pd.keyDown('a')
 
     def l_(self):
-        self.release_all()
+        self.release_left_right()
         print('go left_')
         pd.keyDown('a')
         time.sleep(0.3)
@@ -97,27 +97,37 @@ class my_keyboard:
 
     def r(self):
         if not self.d_key:
-            self.release_all()
+            self.release_left_right()
             self.d_key = True
             print('go right')
             pd.keyDown('d')
 
     def r_(self):
-        self.release_all()
+        self.release_left_right()
         print('go right_')
         pd.keyDown('d')
         time.sleep(0.3)
         pd.keyUp('d')
 
     def up(self):
-        self.release_all()
-        time.sleep(0.1)
         if not self.w_key:
-            self.release_all()
+            # self.release_up()
             self.w_key = True
             print('go up')
             pd.keyDown('w')
-            time.sleep(0.3)
+
+    def release_up(self):
+        if self.w_key:
+            self.w_key = False
+            pd.keyUp('w')
+
+    def release_left_right(self):
+        if self.a_key:
+            self.a_key = False
+            pd.keyUp('a')
+        if self.d_key:
+            self.d_key = False
+            pd.keyUp('d')
 
     def release_all(self):
         if self.w_key:
@@ -138,28 +148,26 @@ ky = my_keyboard()
 def keyboard_ctrl():
     global target_x1, target_x2, target_y1, target_y2, e_trigger
     while(True):
-        if detect_time >= 2:
-            wait_time = int(detect_time/2)
-        else:
-            wait_time = 1
-        time.sleep(wait_time/1000.0)
-
         if end_game:
             print('ready to end game keyboard')
             break
         
         if not start_flag:
-            print('wait start keyboard')
             time.sleep(0.5)
             continue
 
-        if e_trigger:                               # 偵測到採集時
-            ky.press_e()
-            time.sleep(0.5)
-        if target_x1 == -1 and target_y1 == -1:   # 沒偵測到任何東西
+        wait_time = int(detect_time/2)
+        time.sleep(wait_time/1000.0)
+
+        # if e_trigger:                               # 偵測到採集時
+        #     ky.press_e()
+        #     time.sleep(0.5)
+        if target_x1 == -1 and target_y1 == -1 and target_x2 == -1 and target_y2 == -1:   # 沒偵測到任何東西
             # sight_l()
             ky.l()
+            ky.release_up()
         else:
+            ky.up()
             human_x = (human_x1 + human_x2)/2
             human_y = (human_y1 + human_y2)/2
             target_x = (target_x1 + target_x2)/2
@@ -169,36 +177,29 @@ def keyboard_ctrl():
                 if target_x2 <= human_x1:   # 可往左邊走
                     if (human_x1 - target_x2) > 25:
                         ky.l()
-                    elif (human_x1 - target_x2) > 10:
+                    elif (human_x1 - target_x2) > 5:
                         ky.l_()
-                    else:
-                        ky.up()
                 else:                       # 重疊了
-                    if (target_x2 - human_x1) > 10:
+                    if (target_x2 - human_x1) > 15:
                         ky.r_()
-                    else:
-                        ky.up()
             else:                           # target在人物右邊
                 if human_x2 <= target_x1:   # 可往右邊走
                     if (human_x2 - target_x1) > 25:
                         ky.r()
-                    elif (human_x2 - target_x1) > 10:
+                    elif (human_x2 - target_x1) > 5:
                         ky.r_()
-                    else:
-                        ky.up()
                 else:                       # 重疊了
-                    if (target_x1 - human_x2) > 10:
+                    if (target_x1 - human_x2) > 15:
                         ky.l_()
-                    else:
-                        ky.up()
 
 def run_model():
     print('in run_model')
     global img_PIL, tk_img, imgshow, detect_time, cv_enable, target_x1, target_x2, target_y1, target_y2, e_trigger
 
     cou = 0
-    tracking_bbox = (0,0,0,0)    # (x1, y1, w, h)
+    tracking_bbox = (10, 10, 100, 100)    # (x1, y1, w, h)
     tracker = TrackerCSRT_create()
+    tracker.init(cvtColor(np.array(img_PIL), COLOR_RGB2BGR), tracking_bbox)
 
     while(True):
         if end_game:
@@ -222,7 +223,7 @@ def run_model():
         
         rectangle(frame, (human_x1, human_y1), (human_x2, human_y2), (0, 255, 0), 2, 1)
 
-        if cou%5 == 0:
+        if cou%6 == 0:
             results = model(img_PIL, size=640)  # includes NMS
             bboxs = results.pandas().xyxy[0]
 
@@ -239,7 +240,7 @@ def run_model():
             target_x2_tmp = 0
             target_y1_tmp = 0
             target_y2_tmp = 0
-            e_trigger = False
+            # e_trigger = False
             
             
             if not bboxs.empty:
@@ -280,33 +281,38 @@ def run_model():
                             tracking_bbox = (x1, y1, x2-x1, y2-y1)
                 
                 if bbox_E_area != 999999:
-                    e_trigger = True
-                    # ky.press_e()
+                    # e_trigger = True
                     rectangle(frame, (bbox_E[0], bbox_E[1]), (bbox_E[2], bbox_E[3]), (0, 0, 255), 2, 1)
 
-                if min_dist_tracking < tracking_bbox_size:     # 若 tracking bbox 跟最近的 yolo bbox 的距離小於 tracking bbox 的大小，就可以用 yolo bbox 更新 tracking bbox 的位置
-                    tracker = TrackerCSRT_create()
-                    tracker.init(track_img, tracking_bbox)
-                    print('tracking box update by yolo')
-
-                    target_x1 = tracking_bbox[0]
-                    target_x2 = tracking_bbox[0] + tracking_bbox[2]
-                    target_y1 = tracking_bbox[1]
-                    target_y2 = tracking_bbox[1] + tracking_bbox[3]
-                    rectangle(frame, (target_x1, target_y1), (target_x2, target_y2), (255, 255, 0), 2, 1)
+                if target_x1_tmp == 0 and target_y1_tmp == 0 and target_x2_tmp == 0 and target_y2_tmp == 0:  # 若沒偵測到東西
+                    target_x1 = -1
+                    target_x2 = -1
+                    target_y1 = -1
+                    target_y2 = -1
                 else:
-                    tracker = TrackerCSRT_create()
-                    tracker.init(track_img, (target_x1_tmp, target_y1_tmp, target_x2_tmp-target_x1_tmp, target_y2_tmp-target_y1_tmp))
-                    target_x1 = target_x1_tmp
-                    target_x2 = target_x2_tmp
-                    target_y1 = target_y1_tmp
-                    target_y2 = target_y2_tmp
-                    rectangle(frame, (target_x1, target_y1), (target_x2, target_y2), (255, 255, 0), 2, 1)
+                    if min_dist_tracking < tracking_bbox_size:     # 若 tracking bbox 跟最近的 yolo bbox 的距離小於 tracking bbox 的大小，就可以用 yolo bbox 更新 tracking bbox 的位置
+                        tracker = TrackerCSRT_create()
+                        tracker.init(track_img, tracking_bbox)
+                        # print('tracking box update by yolo')
 
-                if cou == 0:    # begining
-                    # tracker = TrackerCSRT_create()
-                    tracking_bbox = (target_x1, target_y1, target_x2-target_x1, target_y2-target_y1)
-                    tracker.init(track_img, tracking_bbox)
+                        target_x1 = tracking_bbox[0]
+                        target_x2 = tracking_bbox[0] + tracking_bbox[2]
+                        target_y1 = tracking_bbox[1]
+                        target_y2 = tracking_bbox[1] + tracking_bbox[3]
+                        rectangle(frame, (target_x1, target_y1), (target_x2, target_y2), (255, 255, 0), 2, 1)
+                    else:                                          # tracking 更新到全新的位置
+                        tracker = TrackerCSRT_create()
+                        tracker.init(track_img, (target_x1_tmp, target_y1_tmp, target_x2_tmp-target_x1_tmp, target_y2_tmp-target_y1_tmp))
+                        # print(track_img.size, target_x1_tmp, target_y1_tmp, target_x2_tmp, target_y2_tmp)
+                        target_x1 = target_x1_tmp
+                        target_x2 = target_x2_tmp
+                        target_y1 = target_y1_tmp
+                        target_y2 = target_y2_tmp
+                        rectangle(frame, (target_x1, target_y1), (target_x2, target_y2), (255, 255, 0), 2, 1)
+
+                    # if cou == 0:    # begining
+                    #     tracking_bbox = (target_x1, target_y1, target_x2-target_x1, target_y2-target_y1)
+                    #     tracker.init(track_img, tracking_bbox)
             else:
                 target_x1 = -1
                 target_x2 = -1
@@ -339,7 +345,8 @@ def run_model():
 
         if cv_enable:
             putText(frame, str(fps), (50, 50), FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
-            img_PIL = Image.fromarray(cvtColor(frame, COLOR_BGR2RGB))
+            frame_resize = resize(frame, (640, 360), interpolation=INTER_AREA)
+            img_PIL = Image.fromarray(cvtColor(frame_resize, COLOR_BGR2RGB))
             tk_img = ImageTk.PhotoImage(img_PIL)
             imgshow.configure(image=tk_img)
 
@@ -353,13 +360,13 @@ def run_model():
 
 def speed_low():
     global detect_time, speed_lab
-    detect_time += 50
+    detect_time += 20
     speed_lab.configure(text='inference speed :' + str(detect_time))
 
 def speed_fast():
     global detect_time, speed_lab
-    if detect_time != 1:
-        detect_time -= 50
+    if detect_time >= 22:
+        detect_time -= 20
     speed_lab.configure(text='inference speed :' + str(detect_time))
 
 def on_closing():
@@ -439,8 +446,8 @@ class task:
 
             left = gta_.left + offset
             top = gta_.top + offset+20
-            w = 800
-            h = 600
+            w = 1280
+            h = 720
             print(left, top, w, h)
 
             gta_.activate()
@@ -487,7 +494,7 @@ def eee():
             continue
 
         pd.press('e')
-        time.sleep(0.3)
+        time.sleep(0.25)
 ########################### main loop ###########################
 
 t = Thread(target = run_model)
@@ -498,42 +505,43 @@ t4 = Thread(target=eee)
 
 app = tk.Tk()
 app.title('MY_GTA')
-app.geometry('1050x820+5+5')
+app.geometry('750x650+5+5')
 
 my_time = tk.Label(app, text = '0:00:00')
-my_time.grid(column=0, row=0, ipadx=5, pady=5, sticky=tk.W+tk.N)
+my_time.place(x=10, y=10)
 
-labelHeight = tk.Label(app, text = "Height Ratio")
-labelHeight.grid(column=0, row=1, ipadx=5, pady=5, sticky=tk.W+tk.S)
+labelHeight = tk.Label(app, text = "Test")
+labelHeight.place(x=10, y=60)
 
 start_button = tk.Button(app, text = 'Start', width=15, height=2, command=lambda: thread_func('start_game'))
-start_button.grid(column=0, row=2, pady=10, sticky=tk.W)
+start_button.place(x=10, y=110)
 
-stop_button = tk.Button(app, text = 'Stop', width=15, height=2, command=lambda: thread_func('stop_game'))
-stop_button.grid(column=1, row=2, pady=10, sticky=tk.W)
+# stop_button = tk.Button(app, text = 'Stop', width=15, height=2, command=lambda: thread_func('stop_game'))
+# stop_button.grid(column=1, row=2, pady=10, sticky=tk.W)
 
 speed_slow_btn = tk.Button(app, text = 'slow', width=15, height=2, command=speed_low)
-speed_slow_btn.grid(column=0, row=3, pady=10, sticky=tk.W)
+speed_slow_btn.place(x=10, y=160)
 
 speed_lab = tk.Label(app, text = 'infer speed :' + str(detect_time))
-speed_lab.grid(column=1, row=3, pady=10, sticky=tk.W)
+speed_lab.place(x=160, y=170)
 
 speed_fast_btn = tk.Button(app, text = 'fast', width=15, height=2, command=speed_fast)
-speed_fast_btn.grid(column=2, row=3, pady=10, sticky=tk.W)
+speed_fast_btn.place(x=310, y=160)
 
-tmp = ImageTk.PhotoImage(img_PIL)
+new_img = img_PIL.resize((640, 360))
+tmp = ImageTk.PhotoImage(new_img)
 imgshow = tk.Label(app, image=tmp)
-imgshow.grid(column=0, row=4, pady=10, sticky=tk.W)
+imgshow.place(x=10, y=260)
 
 img_on_off_btn = tk.Button(app, text = 'ON/OFF', width=15, height=2, command=lambda: thread_func('img_on_off'))
-img_on_off_btn.grid(column=1, row=4, pady=10, sticky=tk.W)
+img_on_off_btn.place(x=10, y=210)
 
 app.protocol("WM_DELETE_WINDOW", Thread(target=on_closing).start)
 
 
 
 t.start()
-# t2.start()
+t2.start()
 t3.start()
 t4.start()
 app.mainloop()
@@ -541,7 +549,7 @@ print('app end')
 
 t.join()
 print('while end')
-# t2.join()
+t2.join()
 print('keyboard end')
 t3.join()
 
